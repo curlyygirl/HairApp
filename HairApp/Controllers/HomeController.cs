34df220591e6f67f4a62e3b7f4f9ee9f62e5faf3
Dominika Rybska <rybskadominika@gmail.com>
2020-11-24 13:09:03 +0100

    first shape

---
+++ HairApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HairApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hair_Quiz()
+        {
+            return View();
+        }
+
+        public ActionResult Washing_Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult CheckIngredients()
+        {
+            return View();
+        }
+    }
+}
---
+++ HairApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HairApp.MvcApplication" Language="C#" %>
